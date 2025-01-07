8430569aae889f8a9219a09b4e24727aa7d37593
M Iqbal Praditya Oscar <89016678+miqbalpo@users.noreply.github.com>
2025-01-07 22:15:56 +0700

    gtw

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['user' => 'miqbalpo']);
 });
 
 Route::get('/login', function () {
     return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/test', function () {
+    return view('test');
 });
 
