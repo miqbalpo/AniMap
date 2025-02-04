a66265a49e9a18ea8b62f02f3c67e6b3094f2786
M Iqbal Praditya Oscar <89016678+miqbalpo@users.noreply.github.com>
2025-02-04 15:46:46 +0700

    added middleware + chart logic + fixed navbar pfp placeholder

--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,14 @@ Route::post('/login/submit', [LoginController::class, 'login'])->name('login.sub
 Route::get('/register', function () {
     return view('register', ['title'=> 'Sign Up to AniMap']);
 })->name('register');
+
 Route::post('/register/create-account', [RegisterController::class, 'register'])->name('register.create-account');
 
 
 // Routes with Middleware
 Route::middleware(['auth'])->group(function () {
-    // Route::get('/account-info', function () {
-    //     return view('account-info', ['title' => 'Account Information']);
-    // })->name('account-info');
+    // Anime Bookmark Route
+    Route::post('/anime-info/update', [BookmarkController::class, 'updateAnimeList'])->name('anime-list.update');
 
     Route::get('/account-info', [AccountController::class, 'account_info'])->name('account-info');
 
@@ -44,7 +44,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/anime-list', function () {
         return view('anime-list', ['title' => 'My Anime List']);
     })->name('anime-list');
-    
+
     Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 });
 
@@ -53,9 +53,6 @@ Route::get('/search-results', [SearchController::class, 'browse'])->name('search
 
 // Anime Info Route
 Route::get('/anime-info/{id}', [AnimeController::class, 'anime_info'])->name('anime-info');
-
-// Anime Bookmark Route
-Route::post('/anime-info/update', [BookmarkController::class, 'updateAnimeList'])->name('anime-list.update');
 
 Route::get('/anime-list/status/{mal_id}', [BookmarkController::class, 'getCurrentStatus'])->middleware('auth');
 
